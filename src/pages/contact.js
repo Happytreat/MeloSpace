8b1aa21552cd24d124a381c0a4ca6adefdfe68f0
melodiessim98 <melodiessim98@gmail.com>
2020-05-12 18:50:33 +0800

    Add Medium link to Contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,37 +1,41 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from "react";
+import { graphql } from "gatsby";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 class BlogAbout extends React.Component {
   render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
+    const { data } = this.props;
+    const siteTitle = data.site.siteMetadata.title;
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="Contact" />
-        <h2>
-          Contact Me
-        </h2>
+        <h2>Contact Me</h2>
         <h5>Add me on: </h5>
-        <p style={{alignContent: 'center'}}>
-          LinkedIn: <a href="https://www.linkedin.com/in/melodies-sim-69b805100/">@MelodiesSim</a>
+        <p style={{ alignContent: "center" }}>
+          LinkedIn:{" "}
+          <a href="https://www.linkedin.com/in/melodies-sim-69b805100/">
+            @MelodiesSim
+          </a>
+        </p>
+        <p>
+          Follow me on <a href="https://medium.com/@melodiessim98">Medium</a> to
+          stay updated to my latest posts!{" "}
         </p>
       </Layout>
-    )
+    );
   }
 }
 
-export default BlogAbout
-
+export default BlogAbout;
 
 export const pageQuery = graphql`
-    query {
-        site {
-            siteMetadata {
-                title
-            }
-        }
+  query {
+    site {
+      siteMetadata {
+        title
+      }
     }
-`
+  }
+`;
