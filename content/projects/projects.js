012eadc9ecb4ab6df5ba5fe82cdbf6762cdfe2eb
melodiessim98 <melodiessim98@gmail.com>
2020-05-12 18:46:49 +0800

    Update project - Bold

--- content/projects/projects.js
+++ content/projects/projects.js
@@ -1,66 +1,83 @@
 const Projects = [
   {
-    title: 'UPlan',
-    subtitle: 'A visual univeristy planning web app includes features such as module tagging and adding goals to each semester',
-    description: 'In progress: Built using Serverless Framework, NodeJS and React-Redux.',
-    color: 'pink',
-    github: 'https://github.com/Happytreat/UPlan2.0',
-    website: 'https://uplan.netlify.com/',
+    title: "Bold",
+    subtitle:
+      "An idea curation app centered around ideas, for creative thinkers.",
+    description:
+      "Bold is an iOS app that lets you effortlessly collect and search ideas of any kind—notes, screenshots, YouTube videos, links—all in a single place.",
+    color: "blue",
+    more:
+      "https://docs.google.com/document/d/10ULywgo8-dMbjTfEQs3MmtISFyPuUa8jQAHoRivowkU/edit?usp=sharing",
   },
   {
-    title: 'Patronage Collectibles',
-    subtitle: 'A decentralised crowdfunding platform with crypto-collectibles and\n' +
-      '            Harberger taxes',
-    description: 'Built using Solidity, React-Drizzle, Truffle and Ganache',
-    github: 'https://github.com/Happytreat/PatronageCollectibles',
-    more: 'https://devpost.com/software/patronage-collectibles',
-    color: 'blue',
+    title: "UPlan",
+    subtitle:
+      "A visual univeristy planning web app includes features such as module tagging and adding goals to each semester",
+    description:
+      "In progress: Built using Serverless Framework, NodeJS and React-Redux.",
+    color: "pink",
+    github: "https://github.com/Happytreat/UPlan2.0",
+    website: "https://uplan.netlify.com/",
   },
   {
-    title: 'NUS Hangs',
-    subtitle: 'A desktop schedule manager application aim to help small to mid-size study/interest groups in NUS organise meetups based on timetables',
-    description: 'Java',
-    github: 'https://github.com/CS2103-AY1819S1-F11-4/main',
-    more: 'https://cs2103-ay1819s1-f11-4.github.io/main/',
-    color: 'green',
+    title: "Patronage Collectibles",
+    subtitle:
+      "A decentralised crowdfunding platform with crypto-collectibles and\n" +
+      "            Harberger taxes",
+    description: "Built using Solidity, React-Drizzle, Truffle and Ganache",
+    github: "https://github.com/Happytreat/PatronageCollectibles",
+    more: "https://devpost.com/software/patronage-collectibles",
+    color: "yellow",
   },
   {
-    title: 'Implement the Blockchain With Python',
-    subtitle: 'Implementing a peer-to-peer Blockchain in Python.',
-    description: 'Python',
-    github: 'https://github.com/Happytreat/blockchain',
-    color: 'yellow',
+    title: "NUS Hangs",
+    subtitle:
+      "A desktop schedule manager application aim to help small to mid-size study/interest groups in NUS organise meetups based on timetables",
+    description: "Java",
+    github: "https://github.com/CS2103-AY1819S1-F11-4/main",
+    more: "https://cs2103-ay1819s1-f11-4.github.io/main/",
+    color: "green",
   },
   {
-    title: 'Hodlnaut App Site',
-    subtitle: 'Contributed to the UI and back-end API of Hodlnaut app site. Independently integrated the Know Your Customer (KYC) system ' +
-      'into the app.',
-    description: 'Built with React-Redux, NodeJS and ExpressJS',
-    website: 'https://app.hodlnaut.com/demo',
-    color: 'pink',
+    title: "Implement the Blockchain With Python",
+    subtitle: "Implementing a peer-to-peer Blockchain in Python.",
+    description: "Python",
+    github: "https://github.com/Happytreat/blockchain",
+    color: "yellow",
   },
   {
-    title: 'Hodlnaut Landing Page',
-    subtitle: 'Implement the landing page of startup Hodlnaut.',
-    description: 'Designed with HTML, CSS',
-    website: 'https://www.hodlnaut.com',
-    color: 'green',
+    title: "Hodlnaut App Site",
+    subtitle:
+      "Contributed to the UI and back-end API of Hodlnaut app site. Independently integrated the Know Your Customer (KYC) system " +
+      "into the app.",
+    description: "Built with React-Redux, NodeJS and ExpressJS",
+    website: "https://app.hodlnaut.com/demo",
+    color: "pink",
   },
   {
-    title: 'MeloSpace',
-    subtitle: 'This website! First personal website built with a static site generator. See if you can find my very first webpage on Github :p',
-    description: 'Built using Gatsby and React and hosted on Netlify',
-    github: 'https://github.com/Happytreat/myWebsite',
-    color: 'yellow',
+    title: "Hodlnaut Landing Page",
+    subtitle: "Implement the landing page of startup Hodlnaut.",
+    description: "Designed with HTML, CSS",
+    website: "https://www.hodlnaut.com",
+    color: "green",
   },
   {
-    title: 'Face Recognition from Image',
-    subtitle: 'A simple webpage which identifies faces from images and logs how many times a logged in user has submitted an image.' +
-      ' First website built using React-NodeJS stack.',
-    description: 'Built using React-Redux, NodeJS Express',
-    github: 'https://github.com/Happytreat/facerecognition',
-    color: 'blue',
+    title: "MeloSpace",
+    subtitle:
+      "This website! First personal website built with a static site generator. See if you can find my very first webpage on Github :p",
+    description: "Built using Gatsby and React and hosted on Netlify",
+    github: "https://github.com/Happytreat/myWebsite",
+    color: "yellow",
   },
-]
+  {
+    title: "Face Recognition from Image",
+    subtitle:
+      "A simple webpage which identifies faces from images and logs how many times a logged in user has submitted an image." +
+      " First website built using React-NodeJS stack.",
+    description: "Built using React-Redux, NodeJS Express",
+    github: "https://github.com/Happytreat/facerecognition",
+    color: "blue",
+  },
+];
 
 export default Projects;
