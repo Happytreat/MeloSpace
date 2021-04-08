2c5af6fc9bdbdf035c05d01c84802b1d5b0f437d
Melodies Sim <cowfats@gmail.com>
2021-04-08 16:52:04 +0800

    Add paper summary structure to Notes

--- src/pages/notes.js
+++ src/pages/notes.js
@@ -22,8 +22,21 @@ class BlogIndex extends React.Component {
             collate key points and argument made in these notes. I will also frequently refer to videos or 
             secondary materials for more context. 
             The notes in this section, hence, acts like a collation of different insights from various
-            source on the subject matter. 
+            sources on the subject matter. 
         </div>
+        <h3>Structure of paper summary</h3>
+        <p>
+        For paper summaries, I try to prioritize understanding the intuitions behind the technology over 
+        memorizing the nitty-gritty details. In particular, I will focus on the following questions: 
+        <br></br>
+        <ol>
+          <li>What is the big picture (architecture)?</li> 
+          <li>What is the problem the authors wish to solve? </li>
+          <li>How is the solution implemented in practice? (How does it differ from the theory?)</li>
+          <li>What are the main advantages of the solution?</li>
+          <li>What are some shortcomings of the solution?</li>
+        </ol>
+        </p>
         <br></br>
         <hr></hr>
         <h3>1. MIT 6.824: Distributed Systems <a href="https://pdos.csail.mit.edu/6.824/schedule.html">[2020 Course Schedule]</a></h3>
